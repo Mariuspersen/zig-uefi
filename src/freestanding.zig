1bbfe45c9b8ae19d80f5ecb0b091f44280c5d3f7
Marius Alexandersen <marius_kule@hotmail.com>
2025-03-08 04:28:14 +0100

    attempting to add stack trace to panic screen, while keeping gdb debugging

--- src/freestanding.zig
+++ src/freestanding.zig
@@ -17,6 +17,7 @@ fn main() !void {
     const writer = try io.UART_OUT.writer();
     const screen = graphics.writer();
     try screen.print("Hello world from {any}!\n", .{@This()});
+    try screen.print("Stripped? {any}\n", .{@import("builtin").strip_debug_info});
     try writer.writeAll("Hello World!\n");
 
     for (0..graphics.context.mode.max_mode) |i| {
@@ -36,28 +37,54 @@ fn main() !void {
     @panic("Oopsies");
 }
 
+fn makeDwarfSection(start: *u8, end: *u8) std.dwarf.DwarfInfo.Section {
+    const ptr = @intFromPtr(start);
+    const size = @intFromPtr(end)-@intFromPtr(start);
+    return .{
+        .data = @as([*]u8, @ptrFromInt(ptr))[0..size],
+        .owned = false,
+    };
+}
+
 pub fn panic(msg: []const u8, stack_trace: ?*builtin.StackTrace, return_address: ?usize) noreturn {
     const RED = 0x00FF0000;
     for (graphics.buffer) |*pixel| {
         pixel.* = RED;
     }
 
-    const uart = io.UART_OUT.writer() catch while (true) {};
+    const uart = io.UART_OUT.writer() catch loopForever();
     const screen = graphics.writer();
 
-    graphics.cursor = .{ .x = 20, .y = 20};
+    graphics.cursor = .{ .x = 20, .y = 20 };
     graphics.background = RED;
     graphics.foreground = 0xFFFFFFFF;
 
-    uart.print("Kernel panicked: {s}\n", .{msg}) catch while (true) {};
-    screen.print("Kernel panicked: {s}\n", .{msg}) catch while (true) {};
+    uart.print("Kernel panicked: {s}\n", .{msg}) catch loopForever();
+    screen.print("Kernel panicked: {s}\n\n", .{msg}) catch loopForever();
     if (stack_trace) |strace| {
-        strace.format("", .{}, uart) catch while (true) {};
-        strace.format("", .{}, screen) catch while (true) {};
+        graphics.cursor.x = 20;
+        strace.format("", .{}, uart) catch loopForever();
+        strace.format("", .{}, screen) catch loopForever();
     }
     if (return_address) |ret_addr| {
-        uart.print("return_address: 0x{X}\n", .{ret_addr}) catch while (true) {};
-        screen.print("return_address: 0x{X}\n", .{ret_addr}) catch while (true) {};
+        graphics.cursor.x = 20;
+        uart.print("return_address: 0x{X}\n", .{ret_addr}) catch loopForever();
+        screen.print("return_address: 0x{X}\n", .{ret_addr}) catch loopForever();
     }
-    while (true) {}
+
+    var stackIt = std.debug.StackIterator.init(return_address, null);
+    while (stackIt.next()) |address| {
+        graphics.cursor.x = 40;
+        screen.print("0x{X}\n", .{address}) catch loopForever();
+    }
+
+    while (true) {
+        @breakpoint();
+    }
+}
+
+inline fn loopForever() noreturn {
+    while (true) {
+        @breakpoint();
+    }
 }
